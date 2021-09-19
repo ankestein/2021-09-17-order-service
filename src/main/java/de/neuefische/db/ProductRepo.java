1613f10265f07a509b7404e7ce346452a6f9b344
Anke <a.stein@posteo.de>
2021-09-19 07:36:32 +0200

    Create classes Product and ProductRepo with methods

--- src/main/java/de/neuefische/db/ProductRepo.java
+++ src/main/java/de/neuefische/db/ProductRepo.java
@@ -1,4 +1,34 @@
 package de.neuefische.db;
 
+import de.neuefische.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductRepo {
+
+    private List<Product> products;
+
+    // constructors
+    public ProductRepo(List<Product> products){
+        this.products = new ArrayList<>(products);
+    }
+
+    public ProductRepo(){
+    };
+
+
+    public List<Product> listProducts(){
+        return products;
+    }
+
+    public Product getProductById(int id){
+        for (Product product : products){
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
+    }
+
 }
