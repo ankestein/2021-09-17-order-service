957c14b989f069adbbbe0f9fddcd09a38865c8b4
Anke <a.stein@posteo.de>
2021-09-21 15:30:43 +0200

    Use unmodifiableList in constructor; return Optional<Product> instead of Product in getProductById

--- src/main/java/de/neuefische/db/ProductRepo.java
+++ src/main/java/de/neuefische/db/ProductRepo.java
@@ -3,7 +3,9 @@ package de.neuefische.db;
 import de.neuefische.model.Product;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public class ProductRepo {
 
@@ -11,23 +13,22 @@ public class ProductRepo {
 
     // constructors
     public ProductRepo(List<Product> products) {
-        this.products = new ArrayList<>(products);
+        this.products = Collections.unmodifiableList(products);
+        // new ArrayList() would be modifiable, but we want to protect list from being modified from the outside.
+        // List() is also unmodifiable, but "new List()" produces error "'List' is abstract, cannot be instantiated"
     }
 
     public ProductRepo() {
     }
 
-    ;
-
-
     public List<Product> listProducts() {
         return products;
     }
 
-    public Product getProductById(int id) {
+    public Optional<Product> getProductById(int id) {
         for (Product product : products) {
-            if (product.getId() == id) {
-                return product;
+            if (product.getId().equals(id)) {
+                return Optional.of(product);
             }
         }
         return null;
