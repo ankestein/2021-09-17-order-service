0e4b75674b804ef2cac1544fe65d3426f672eecb
Anke <a.stein@posteo.de>
2021-09-22 09:52:46 +0200

    Change int ids to Strings in tests

--- src/test/java/neuefische/db/OrderRepoTest.java
+++ src/test/java/neuefische/db/OrderRepoTest.java
@@ -16,8 +16,8 @@ class OrderRepoTest {
     @DisplayName("listOrders() should return a list of all orders")
     public void testListOrders(){
         // given
-        Order order1 = new Order(1, new ArrayList<Product>(List.of(new Product (111, "bookA"), new Product(21, "bookB"))));
-        Order order2 = new Order(2, new ArrayList<Product>(List.of(new Product(222, "pencil"))));
+        Order order1 = new Order("1", new ArrayList<Product>(List.of(new Product (111, "bookA"), new Product(21, "bookB"))));
+        Order order2 = new Order("2", new ArrayList<Product>(List.of(new Product(222, "pencil"))));
         OrderRepo orderRepo = new OrderRepo(List.of(order1, order2));
         // when
         List<Order> actual = orderRepo.listOrders();
@@ -42,11 +42,11 @@ class OrderRepoTest {
     @DisplayName("getOrder() should return order with the given id")
     public void testGetOrder(){
         // given
-        Order order1 = new Order(1, new ArrayList<Product>(List.of(new Product (111, "bookA"), new Product(21, "bookB"))));
-        Order order2 = new Order(2, new ArrayList<Product>(List.of(new Product(222, "pencil"))));
+        Order order1 = new Order("1", new ArrayList<Product>(List.of(new Product (111, "bookA"), new Product(21, "bookB"))));
+        Order order2 = new Order("2", new ArrayList<Product>(List.of(new Product(222, "pencil"))));
         OrderRepo orderRepo = new OrderRepo(List.of(order1, order2));
         // when
-        Order actual = orderRepo.getOrder(2);
+        Order actual = orderRepo.getOrder("2");
         // then
         Order expected = order2;
         Assertions.assertEquals(expected, actual);
@@ -56,26 +56,26 @@ class OrderRepoTest {
     @DisplayName("getOrder() should return null if no order with matching id exists")
     public void testGetNonExistingOrder(){
         // given
-        Order order1 = new Order(1, new ArrayList<Product>(List.of(new Product (111, "bookA"), new Product(21, "bookB"))));
-        Order order2 = new Order(2, new ArrayList<Product>(List.of(new Product(222, "pencil"))));
+        Order order1 = new Order("1", new ArrayList<Product>(List.of(new Product (111, "bookA"), new Product(21, "bookB"))));
+        Order order2 = new Order("2", new ArrayList<Product>(List.of(new Product(222, "pencil"))));
         OrderRepo orderRepo = new OrderRepo(List.of(order1, order2));
         // when
-        Order actual = orderRepo.getOrder(3);
+        Order actual = orderRepo.getOrder("3");
         // then
         Order expected = null;
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    @DisplayName("addOrders() should add an order to OrderRepo")
+    @DisplayName("addOrder() should add an order to OrderRepo")
     public void testAddOrder(){
         // given
-        Order order1 = new Order(1, new ArrayList<Product>(List.of(new Product (111, "bookA"), new Product(21, "bookB"))));
-        Order order2 = new Order(2, new ArrayList<Product>(List.of(new Product(222, "pencil"))));
+        Order order1 = new Order("1", new ArrayList<Product>(List.of(new Product (111, "bookA"), new Product(21, "bookB"))));
+        Order order2 = new Order("2", new ArrayList<Product>(List.of(new Product(222, "pencil"))));
         OrderRepo orderRepo = new OrderRepo(List.of(order1, order2));
-        Order order3 = new Order(3, new ArrayList<Product>(List.of(new Product(333, "computer"))));
+        Order order3 = new Order("3", new ArrayList<Product>(List.of(new Product(333, "computer"))));
         // when
-        orderRepo.addOrders(order3);
+        orderRepo.addOrder(order3);
         List<Order> actual = orderRepo.listOrders();
         // then
         List<Order> expected = new ArrayList<>(List.of(order1, order2, order3));
@@ -83,13 +83,13 @@ class OrderRepoTest {
     }
 
     @Test
-    @DisplayName("addOrders() used on an empty OrderRepo should add an order")
+    @DisplayName("addOrder() used on an empty OrderRepo should add an order")
     public void testAddFirstOrder(){
         // given
-        Order order1 = new Order(1, new ArrayList<Product>(List.of(new Product (111, "bookA"), new Product(21, "bookB"))));
+        Order order1 = new Order("1", new ArrayList<Product>(List.of(new Product (111, "bookA"), new Product(21, "bookB"))));
         OrderRepo orderRepo = new OrderRepo();
         // when
-        orderRepo.addOrders(order1);
+        orderRepo.addOrder(order1);
         List<Order> actual = orderRepo.listOrders();
         // then
         List<Order> expected = new ArrayList<>(List.of(order1));
@@ -100,12 +100,12 @@ class OrderRepoTest {
     @DisplayName("addOrder() with an already existing id should throw an exception")
     public void testAddOrderWithSameIdException() {
         // given
-        Order order1 = new Order(1, new ArrayList<Product>(List.of(new Product (111, "bookA"), new Product(21, "bookB"))));
-        Order order2 = new Order(2, new ArrayList<Product>(List.of(new Product(222, "pencil"))));
+        Order order1 = new Order("1", new ArrayList<Product>(List.of(new Product (111, "bookA"), new Product(21, "bookB"))));
+        Order order2 = new Order("2", new ArrayList<Product>(List.of(new Product(222, "pencil"))));
         OrderRepo orderRepo = new OrderRepo(List.of(order1, order2));
         // when
         try {
-            orderRepo.addOrders(new Order(2, new ArrayList<Product>(List.of(new Product(222, "stapler")))));    fail("Exception not thrown!");
+            orderRepo.addOrder(new Order("2", new ArrayList<Product>(List.of(new Product(222, "stapler")))));    fail("Exception not thrown!");
         } catch (RuntimeException e) {
             String actual = e.getMessage();
             String expected = "Cannot place order; order with id 2 already exists.";
@@ -117,11 +117,11 @@ class OrderRepoTest {
     @DisplayName("addOrder() with a list input should add multiple orders to an OrderRepo")
     public void testAddOrderList(){
         // given
-        Order order1 = new Order(1, new ArrayList<Product>(List.of(new Product(111, "bookA"), new Product(21, "bookB"))));
-        Order order2 = new Order(2, new ArrayList<Product>(List.of(new Product(222, "pencil"))));
+        Order order1 = new Order("1", new ArrayList<Product>(List.of(new Product(111, "bookA"), new Product(21, "bookB"))));
+        Order order2 = new Order("2", new ArrayList<Product>(List.of(new Product(222, "pencil"))));
         OrderRepo orderRepo = new OrderRepo(List.of(order1, order2));
-        Order order3 = new Order(3, new ArrayList<Product>(List.of(new Product(333, "computer"))));
-        Order order4 = new Order(4, new ArrayList<Product>(List.of(new Product(111, "bookA"), new Product(115, "keyboard"))));   List<Order> newOrders = new ArrayList<>(List.of(order3, order4));
+        Order order3 = new Order("3", new ArrayList<Product>(List.of(new Product(333, "computer"))));
+        Order order4 = new Order("4", new ArrayList<Product>(List.of(new Product(111, "bookA"), new Product(115, "keyboard"))));   List<Order> newOrders = new ArrayList<>(List.of(order3, order4));
         // when
         orderRepo.addOrders(newOrders);
         List<Order> actual = orderRepo.listOrders();
