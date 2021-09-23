df1ecb9267a12c1e325a2136f2f3053ebb042b2c
Anke <a.stein@posteo.de>
2021-09-23 09:00:38 +0200

    Add tests for addOrder()

--- src/test/java/neuefische/service/ShopServiceTest.java
+++ src/test/java/neuefische/service/ShopServiceTest.java
@@ -1,33 +1,46 @@
 package neuefische.service;
 
+import de.neuefische.db.OrderRepo;
+import de.neuefische.db.ProductRepo;
+import de.neuefische.model.Order;
+import de.neuefische.model.Product;
+import de.neuefische.service.ShopService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class ShopServiceTest {
 
     @Test
     @DisplayName("addOrder() should add an order with given id and products to an OrderRepo")
     public void testAddOrder(){
-        // given
+       //GIVEN
+        List<Product> products = List.of(
+                new Product(1, "piano"),
+                new Product(2, "guitar"),
+                new Product(3, "drums")
+        );
+        ProductRepo productRepo = new ProductRepo(products);
+        OrderRepo orderRepo = new OrderRepo();
+        ShopService shopService = new ShopService(productRepo, orderRepo);
 
-        // when
+        //WHEN
+        Order actual = shopService.addOrder(List.of(1, 3));
 
-        // then
-    }
-
+        //THEN
+        List<Product> expected = List.of(
+                new Product(1, "piano"),
+                new Product(3, "drums")
+        );
+        assertEquals(expected, actual.getProducts());
 }
 
-    @Test
-    @DisplayName("addOrder() used on an empty OrderRepo should add an order with given id and products")
-    public void testAddFirstOrder(){
-        // given
-
-        // when
-
-        // then
-    }
-
-
+/*
     @Test
     @DisplayName("addOrder() with an already existing id should throw an exception")
     public void testAddOrderWithSameIdException(){
@@ -37,17 +50,31 @@ public class ShopServiceTest {
 
         // then
     }
-
+*/
 
 
     @Test
     @DisplayName("addOrder() with non-existing productId should throw an exception")
-    public void testAddOrderWithNonExistingProductIdException(){
-        // given
+    public void testAddOrderNonExistingProduct() {
+        //GIVEN
+        List<Product> products = List.of(
+                new Product(1, "piano"),
+                new Product(2, "guitar"),
+                new Product(3, "drums")
+        );
+        ProductRepo productRepo = new ProductRepo(products);
+        OrderRepo orderRepo = new OrderRepo();
+        ShopService shopService = new ShopService(productRepo, orderRepo);
 
-        // when
-
-        // then
+        try {
+            //WHEN
+            shopService.addOrder(List.of(1, 4));
+            fail("Exception not thrown!");
+        } catch (IllegalArgumentException actual) {
+            //THEN
+            assertEquals("Product with ID 4 not found", actual.getMessage());
+        }
     }
+
 
 }
